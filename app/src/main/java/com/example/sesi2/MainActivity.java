364e8ffd8dd4c30ee93d39dda416ce5636a8b521
Gilangjanuar <gilangjanuar2305@gmail.com>
2021-04-06 20:53:54 +0700

    sesi 5

--- app/src/main/java/com/example/sesi2/MainActivity.java
+++ app/src/main/java/com/example/sesi2/MainActivity.java
@@ -8,12 +8,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import static android.text.TextUtils.isEmpty;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText editTxtSatu;
     private EditText editTxtDua;
     private Button btnHitung;
-    EditText editTxtHasil;
+    private Button btnMinus;
+    private Button btnPlus;
+    private Button btnKali;
+    private Button btnBagi;
+    private EditText editTxtHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +30,72 @@ public class MainActivity extends AppCompatActivity {
         editTxtDua = findViewById(R.id.editTxtDua);
         editTxtHasil = findViewById(R.id.editTxtHasil);
         btnHitung = findViewById(R.id.btnHitung);
+        btnMinus = findViewById(R.id.btnMinus);
+        btnPlus = findViewById(R.id.btnPlus);
+        btnKali = findViewById(R.id.btnKali);
+        btnBagi = findViewById(R.id.btnBagi);
 
         btnHitung.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String angka1 = editTxtSatu.getText().toString();
                 String angka2 = editTxtDua.getText().toString();
-                editTxtHasil.setText(Integer.parseInt(angka1) + Integer.parseInt(angka2));
-//                Toast.makeText(MainActivity.this, "HELLO WORLD", Toast.LENGTH_LONG).show();
+                notNull();
+                editTxtHasil.setText(String.valueOf((Integer.parseInt(angka1) + Integer.parseInt(angka2)*2)));
             }
         });
+
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String angka1 = editTxtSatu.getText().toString();
+                String angka2 = editTxtDua.getText().toString();
+                notNull();
+                editTxtHasil.setText(String.valueOf(Integer.parseInt(angka1) - Integer.parseInt(angka2)));
+            }
+        });
+
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String angka1 = editTxtSatu.getText().toString();
+                String angka2 = editTxtDua.getText().toString();
+                notNull();
+                editTxtHasil.setText(String.valueOf(Integer.parseInt(angka1) + Integer.parseInt(angka2)));
+            }
+        });
+
+        btnKali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String angka1 = editTxtSatu.getText().toString();
+                String angka2 = editTxtDua.getText().toString();
+                notNull();
+                editTxtHasil.setText(String.valueOf(Integer.parseInt(angka1) * Integer.parseInt(angka2)));
+            }
+        });
+
+        btnBagi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String angka1 = editTxtSatu.getText().toString();
+                String angka2 = editTxtDua.getText().toString();
+                notNull();
+                editTxtHasil.setText(String.valueOf(Integer.parseInt(angka1) / Integer.parseInt(angka2)));
+
+            }
+        });
+    }
+
+    private void notNull() {
+        if (isEmpty(editTxtSatu.getText().toString())) {
+            editTxtSatu.setError("WAJIB DIISI");
+            return;
+        }
+
+        if (isEmpty(editTxtDua.getText().toString())) {
+            editTxtDua.setError("WAJIB DIISI");
+            return;
+        }
     }
 }
